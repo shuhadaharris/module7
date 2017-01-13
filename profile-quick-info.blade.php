e1b3d78d6347a72f7e404d7b912456a4ee79d183
atiqahadlina <atiqahadlina1995@gmail.com>
2017-01-13 17:49:34 +0800

    layouts

---
+++ profile-quick-info.blade.php
@@ -0,0 +1,9 @@
+<div class="profile">
+	<div class="profile_pic">
+		<img src="images/picture.jpg" alt="..." class="img-circle profile_img">
+	</div>
+	<div class="profile_info">
+		<span>Welcome,</span>
+		<h2>Jane Doe</h2>
+	</div>
+</div>
---
+++ sidebar-menu.blade.php
@@ -0,0 +1,15 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+  <div class="nav side-menu"></div>
+  <div class="menu_section">
+    <ul class="nav side-menu">
+      <li><a href="/"><i class="fa fa-home"></i> Home </a></li>
+      <li><a><i class="fa fa-edit"></i> Auditing <span class="fa fa-chevron-down"></span></a>
+      <ul class="nav child_menu">
+        <li><a href="auditAccount">Audit Account</a></li>
+        <li><a href="inviteAuditor">Invite Auditor</a></li>
+      </ul></li>
+      <li><a href="version"><i class="fa fa-clone"></i> Version </a></li>
+      <li><a href="auditedAccount"><i class="fa fa-table"></i> Audited Account </a></li>
+    </ul>
+  </div>
+</div>
