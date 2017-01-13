77c5974f97963445ca4605f80d96c8903e9c6ca9
FatinDelailah <fatindelailahadnan.fa@gmail.com>
2017-01-13 17:48:08 +0800

    layouts

---
+++ js.blade.php
@@ -0,0 +1,19 @@
+ <!-- jQuery -->
+<script src="vendors/jquery/dist/jquery.min.js"></script>
+<script src="vendors/select2/dist/js/select2.min.js"></script>
+<!-- Bootstrap -->
+<script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="vendors/nprogress/nprogress.js"></script>
+<!-- Select2 -->
+<!-- Custom Theme Scripts -->
+<script src="build/js/custom.min.js"></script>
+       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
+<!-- Select2 -->
+<script>
+$(document).ready(function() {
+$("#select2_single").select2();
+});
+</script>
---
+++ layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="_token" content="{!! csrf_token() !!}"/>
+        <title>Module 9 - Auditor and Delegate Module</title>
+        <!-- Bootstrap -->
+        <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <!-- Custom Theme Style -->
+            <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
+        <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
+ 
+         <!-- Select2 -->
+        <link href="vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+        <link href="build/css/custom.min.css" rel="stylesheet">
+    </head>
+    <body class="nav-md">
+        <div class="container body">
+            <div class="main_container">
+                <div class="col-md-3 left_col">
+                    <div class="left_col scroll-view">
+                        <div class="navbar nav_title" style="border: 0;">
+                            <a href="index.html" class="site_title"><i class="fa fa-money"></i> <span>Module 9</span></a>
+                        </div>
+                        <div class="clearfix"></div>
+                        @yield('content')
+                    </div>
+                </div>
+               </body>
+        </html>
